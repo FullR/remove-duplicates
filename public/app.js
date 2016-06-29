4b97521eb567e9dc3bd6afbb055de7fc68ffaf0c
James Meyers <James.Meyers919@gmail.com>
2016-06-29 16:38:12 -0700

    Added tests

--- public/app.js
+++ public/app.js
@@ -1,7 +1,9 @@
 const $ = document.querySelector.bind(document);
-const elements = {
+const elements = { // cache elements
   runBenchmarkButton: $(".run-benchmark-button"),
-  benchmarkResultContainer: $(".benchmark-result-container")
+  benchmarkResultContainer: $(".benchmark-result-container"),
+  uniqueTestResult: $(".unique-test-result"),
+  orderTestResult: $(".order-test-result")
 };
 const testData = createTestData();
 
@@ -14,7 +16,8 @@ function removeDuplicates(arr) {
   const uniqueLookupMap = {}; // used to quickly lookup whether or not a value has been encountered
 
   for(let i = 0; i < length; i++) {
-    if(!(arr[i] in uniqueLookupMap)) { // check if value hasn't been encounted yet
+    const value = arr[i];
+    if(!(value in uniqueLookupMap)) { // check if value is unique
       uniqueLookupMap[value] = true;
       unique.push(value);
     }
@@ -26,12 +29,12 @@ function removeDuplicates(arr) {
 function createTestData() {
   const emails = [];
   const duplicates = []; // duplicate emails to be inserted randomly into emails
-  for(let i = 0; i < 50000; i++) {
+  for(let i = 0; i < 50; i++) {
     const email = `${i}${Math.random() * 10000}@${Math.floor(Math.random() * 10000)}.com`; // generates something like "53891.27@38910.com"
     emails.push(email);
   }
   // get random emails to insert as duplicates
-  for(let i = 0; i < 50000; i++) {
+  for(let i = 0; i < 50; i++) {
     duplicates.push(sample(emails));
   }
 
@@ -44,13 +47,39 @@ function createTestData() {
 }
 
 function runBenchmark() {
-  //const emails = createTestData();
-  const {result, elapsed} = time(() => removeDuplicates(testData));
+  const {result, elapsed} = timeFunction(() => removeDuplicates(testData));
   elements.benchmarkResultContainer.textContent = `${elapsed}ms`;
+  runTests(testData, result);
+}
+
+function runTests(input, output) {
+  if(isUnique(output)) {
+    console.log("is unique");
+  } else {
+    console.log("isn't unique");
+  }
+
+  if(isInRelativeOrder(input, output)) {
+    console.log("is in order");
+  } else {
+    console.log("isn't in order");
+  }
+}
+
+// returns true if arr has no duplicate values by checking that each value is the first of its kind
+function isUnique(arr) {
+  return arr.every((value, i) => arr.indexOf(value) === i);
+}
+
+// returns true if uniqueArr is in the same relative order as originalArr
+// In order to detect whether or not uniqueArr retains originalArr's order,
+// I'm checking that each item's index is less than or equal to its first index in the original array
+function isInRelativeOrder(originalArr, uniqueArr) {
+  return uniqueArr.every((value, i) => i <= originalArr.indexOf(value));
 }
 
 // runs fn and returns an object containing the result and the elapsed time in milliseconds it took to execute (synchronously)
-function time(fn) {
+function timeFunction(fn) {
   const startTime = Date.now();
   const result = fn();
   return {
